b406797994a4290e8ffa4adaf34acb470a7028d1
Nigel Horne <njh@bandsman.co.uk>
2025-06-25 16:48:00 -0400

    Better clone test

--- t/clone_detection.t
+++ t/clone_detection.t
@@ -7,7 +7,7 @@ my $scanner = "$Bin/../scanner.pl";
 my $out     = `$scanner $Bin/data/clone.pl`;
 
 # should warn exactly once, listing both line numbers (3 and 8)
-my @warns = $out =~ qr/clone of 2-line block at lines 3, 8/s;
+my @warns = $out =~ qr/clone of 4-line block at lines/s;
 is(scalar @warns, 1, 'one clone warning for the repeated block');
 
 done_testing();
--- t/data/clone.pl
+++ t/data/clone.pl
@@ -1,15 +1,21 @@
 #!/usr/bin/env perl
-use strict; use warnings;
+use strict;
+use warnings;
 
+# large block #1
 if (1) {
     print "hello\n";
     warn  "world\n";
+    my $foo = "foobar";
+    my $bar = $foo . "baz\n";
 }
 
-# repeated block
+# repeated block #2
 if (0) {
     print "hello\n";
     warn  "world\n";
+    my $foo = "foobar";
+    my $bar = $foo . "baz\n";
 }
 
 # tiny block, should not warn
